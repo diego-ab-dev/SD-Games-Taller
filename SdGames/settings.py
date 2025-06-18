8c396544d65017dd099707a21b4e762769a7c537
diego-ab-dev <diego12barra@gmail.com>
2025-06-18 15:20:13 -0400

    Update settings.py
    permitiendo la entrada a EC2

--- SdGames/settings.py
+++ SdGames/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-b!c@#n($tdrw5lt5wqnqncxdtw^w(2=w#3r%)i!d8ip*%6wsto
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.175.239.68', 'localhost', '127.0.0.1']
 
 
 # Application definition
