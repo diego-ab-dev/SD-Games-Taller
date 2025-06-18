ee47991d520dfada8ec788d693ea7b27971883b6
diego-ab-dev <diego12barra@gmail.com>
2025-06-18 16:15:09 -0400

    Update settings.py
    S3 cambio en settings

--- SdGames/settings.py
+++ SdGames/settings.py
@@ -161,7 +161,10 @@ AWS_SECRET_ACCESS_KEY = ''
 AWS_STORAGE_BUCKET_NAME = 'sdgames-media'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
